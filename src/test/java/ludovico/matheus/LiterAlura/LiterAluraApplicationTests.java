73d845dbacd1e8938bf25c52b2e51ddc9b47fd90
Matheus-Ludovico <matheusludovico@gmail.com>
2025-01-27 20:35:35 -0300

    Initial commit

---
+++ src/test/java/ludovico/matheus/LiterAlura/LiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package ludovico.matheus.LiterAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
